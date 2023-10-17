bb40e7c98dcbf87dcd515fe98eaca7cd7f58eeef
dek.haji1@gmail.com <dek.haji1@gmail.com>
2023-10-16 20:33:42 -0500

    added another output

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: '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'
+        OPENAI_API_KEY: ''
     }
 }
--- index.js
+++ index.js
@@ -13,19 +13,29 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
+  const userInput = setupTextarea.value;
    if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
    }
-  fetchBotReply()
+  fetchBotReply(userInput);
+  fetchSynopsis(userInput);
 })
 
- async function fetchBotReply(){
+ async function fetchBotReply(outline){
   const response = await openai.createCompletion({
       model: 'text-davinci-003',
-    prompt: 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: ' + setupTextarea.value + '\nAI:',
+    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and`,
     max_tokens: 60,
   })
    movieBossText.innerText = response.data.choices[0].text.trim();
 }
 
+async function fetchSynopsis(outline) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate a short synopsis for a software developer informatin about "${outline}"`,
+    max_tokens: 700,
+  })
+   document.getElementById('output-text').innerText = response.data.choices[0].text.trim()
+}
